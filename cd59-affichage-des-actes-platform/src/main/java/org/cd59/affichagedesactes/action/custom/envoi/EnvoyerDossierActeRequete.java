21e3d03b4eadd6471b976980a549b7a1b97763ce
DevPAMGit <44770087+DevPAMGit@users.noreply.github.com>
2023-01-13 11:08:56 +0100

    Nettoyage du code
    Nettoyage du code et suppression des fichiers inutilisés.

--- cd59-affichage-des-actes-platform/src/main/java/org/cd59/affichagedesactes/action/custom/envoi/EnvoyerDossierActeRequete.java
+++ cd59-affichage-des-actes-platform/src/main/java/org/cd59/affichagedesactes/action/custom/envoi/EnvoyerDossierActeRequete.java
@@ -4,19 +4,6 @@ package org.cd59.affichagedesactes.action.custom.envoi;
  * Fichier de requêtes pour l'action d'envoi de d'acte.
  */
 public class EnvoyerDossierActeRequete {
-    /**
-     * Requête permettant de récupérer l'acte original du dossier.
-     */
-    public final static String REQUETE_RECHERCHE_ACTE_ORIGINAL = "select * from actes59:docinfos " +
-            "where actes59:typedocument = 'ACTE_ORIGINAL' " +
-            "and IN_TREE('%s')";
-
-    /**
-     * Requête permettant de récupérer les annexes d'actes du dossier.
-     */
-    public final static String REQUETE_RECHERCHE_ANNEXES_ACTE = "select * from actes59:docinfos " +
-            "where actes59:typedocument <> 'ACTE_ORIGINAL' " +
-            "and IN_TREE('%s')";
 
     /**
      * La requête permettant de retrouver tous les dossiers en possibilité d'envoi contenus dans le dossier générale
